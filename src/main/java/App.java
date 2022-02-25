bfeba4df7a08da2300821d62f74da7d0b6c151eb
Nicuta <ciprian.nicuta@elefant.ro>
2022-02-25 14:14:31 +0200

    fix for command line

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,17 +2,39 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println(encrypt("hello"));
 
-        //todo change the "test" to your token
-        System.out.println(decrypt("test"));
+    public static String encryptorPassword = "";
+
+    public static void main(String[] args) {
+        if (args.length > 2) {
+            String command = args[0];
+            encryptorPassword = args[1];
+            String token = args[2];
+
+            if (command.equalsIgnoreCase("encrypt")) {
+                System.out.println(encrypt(token));
+            } else if (command.equalsIgnoreCase("decrypt")) {
+                System.out.println(decrypt(token));
+            } else {
+                printUsage();
+            }
+
+            encryptorPassword = args[0];
+        } else {
+            printUsage();
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("usage:");
+        System.out.println("\tdecrypt [encryptor password] [token]");
+        System.out.println("\tencrypt [encryptor password] [token]");
     }
 
     private static StandardPBEStringEncryptor createEncryptor() {
         final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword("jasypt.encryptor.password");
+        config.setPassword(encryptorPassword);
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
